6401fd5f82650e1bae1edfd5daab36b12fe1f71c
henriquesd <hsd.foz@gmail.com>
2019-04-16 13:52:26 -0300

    Value and Reference Types

---
+++ ValueAndReferenceTypes/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ValueAndReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteValueType();
+
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("-----------------------------");
+
+            ExecuteReferenceType();
+
+            Console.ReadLine();
+        }
+
+        private static void ExecuteValueType()
+        {
+            Console.WriteLine("Example of Value Type");
+
+            int a = 5;
+            int b = 10;
+            Console.WriteLine("a value: " + a);
+            Console.WriteLine("b value: " + b);
+
+            int c = b;
+            Console.WriteLine("c value: " + c);
+            Console.WriteLine("------------");
+
+            c = 3;
+
+            Console.WriteLine("a value: " + a);
+            Console.WriteLine("b value: " + b);
+            Console.WriteLine("c value: " + c);
+        }
+
+        private static void ExecuteReferenceType()
+        {
+            Console.WriteLine("Example of Reference Type");
+            Person pe1 = new Person();
+            pe1.Name = "Luke";
+            Console.WriteLine("Pe1 name is: " + pe1.Name);
+
+            Person pe2 = new Person();
+            pe2.Name = "John";
+            Console.WriteLine("Pe2 name is: " + pe2.Name);
+
+            Person pe3 = pe2;
+            pe3.Name = "Ned";
+            Console.WriteLine("Pe3 name is: " + pe3.Name);
+
+            Console.WriteLine("------------");
+
+            Console.WriteLine("Pe1 name is: " + pe1.Name);
+            Console.WriteLine("Pe2 name is: " + pe2.Name);
+            Console.WriteLine("Pe3 name is: " + pe3.Name);
+        }
+
+        public class Person
+        {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
+}
---
+++ ValueAndReferenceTypes/README.md
@@ -0,0 +1,58 @@
+﻿The .NET Framework stores items in memory in one of two locations:
+- Stack
+- Heap
+
+- Value types are stored on the Stack;
+- Reference types are stored on the Heap (but there are partially stored on the Stack);
+
+-------------------------------------------------------------------------------------------------------------
+
+Value types:
+Each variable is stored on the stack in the order it was created.
+Each variable stores its own data.
+
+A data type is a value type if it holds a data value within its own memory space. It means variables of these data types directly contain their values.
+
+
+Reference types:
+The variable is stored on the stack.
+The data is stored on the heap.
+
+The variable is stored on the stack, but the data for that reference type is stored on the heap, and then there is a pointer from the
+variable on the stack to the heap (we have to use garbage collection or something a little more sotisticated to clean that data out).
+
+Unlike value types, a reference type doesn't store its value directly. Instead, it stores the address where the value is being stored.
+In other words, a reference type contains a pointer to another memory location that holds the data.
+
+-------------------------------------------------------------------------------------------------------------
+
+The following data types are all of value type:
+bool
+byte
+char
+decimal
+double
+enum
+float
+int
+long
+sbyte
+short
+struct
+uint
+ulong
+ushort
+
+
+The following data types are of reference type:
+String
+All arrays, even if their elements are value types
+Class
+Delegates
+
+
+-------------------------------------------------------------------------------------------------------------
+Fonts:
+
+https://www.tutorialsteacher.com/csharp/csharp-value-type-and-reference-type
+https://www.udemy.com/programming-in-microsoft-c-exam-70-483/
