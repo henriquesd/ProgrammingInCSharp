94fa9a84e947a162e132dbea4f18d0de3a021fd4
henriquesd <hsd.foz@gmail.com>
2019-04-26 13:56:08 -0300

    Exceptions

---
+++ Exceptions/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ExecuteWithError();
+            // ExecuteWithoutError();
+            ExecuteWithErrorUsingExceptionClass();
+
+            Console.ReadLine();
+        }
+
+        private static void ExecuteWithErrorUsingExceptionClass()
+        {
+            int a = 4;
+            int b = 0;
+
+            try
+            {
+                Console.WriteLine(a + " divided by " + b + " is: " + a / b);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error has occurred..." + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finally has been called...");
+            }
+
+            Console.WriteLine("...the program continues...");
+        }
+
+        private static void ExecuteWithoutError()
+        {
+            int a = 4;
+            int b = 2;
+
+            try
+            {
+                Console.WriteLine(a + " divided by " + b + " is: " + a / b);
+            }
+            catch
+            {
+                Console.WriteLine("An error has occurred...");
+            }
+            finally
+            {
+                Console.WriteLine("Finally has been called...");
+            }
+
+            Console.WriteLine("...the program continues...");
+        }
+
+        private static void ExecuteWithError()
+        {
+            int a = 4;
+            int b = 0;
+
+            try
+            {
+                Console.WriteLine(a + " divided by " + b + " is: " + a / b);
+            }
+            catch
+            {
+                Console.WriteLine("An error has occurred...");
+            }
+            finally
+            {
+                Console.WriteLine("Finally has been called...");
+            }
+
+            Console.WriteLine("...the program continues...");
+        }
+    }
+}
---
+++ Exceptions/README.md
@@ -0,0 +1,14 @@
+﻿Exceptions
+An exception is any error condition or unexpected behavior that is encountered by an executing program.
+Exceptions can be thrown because of a fault in your code or in code that you call (such as a shared library), unavailable operating system resources,
+unexpected conditions that the runtime encounters (such as code that can't be verified), and so on. Your application can recover from some of these
+conditions, but not from others. Although you can recover from most application exceptions, you can't recover from most runtime exceptions.
+
+In .NET, an exception is an object that inherits from the System.Exception class. An exception is thrown from an area of code where a problem has occurred.
+The exception is passed up the stack until the application handles it or the program terminates.
+
+-------------------------------------------------------------------------------------------------------------
+Font:
+
+https://www.udemy.com/programming-in-microsoft-c-exam-70-483
+https://docs.microsoft.com/en-us/dotnet/standard/exceptions/
