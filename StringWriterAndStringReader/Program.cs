4f330ba6b0bb1501d37166475e0b3f5dcb0f694d
HenriqueSD <hsd.foz@gmail.com>
2019-04-26 22:21:45 -0300

    StringWriter and StringReader

---
+++ StringWriterAndStringReader/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace StringWriterAndStringReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringReadWrite srw = new StringReadWrite();
+            Console.ReadLine();
+        }
+    }
+
+    public class StringReadWrite
+    {
+        StringBuilder sb = new StringBuilder();
+
+        public StringReadWrite()
+        {
+            WriteData();
+            ReadData();
+        }
+
+        private void WriteData()
+        {
+            StringWriter sw = new StringWriter(sb);
+
+            Console.WriteLine("Please enter your first and last name...");
+            string name = Console.ReadLine();
+
+            sw.WriteLine("Name: " + name);
+
+            // Close the sw stream object;
+            sw.Flush();
+            sw.Close();
+        }
+
+        private void ReadData()
+        {
+            StringReader sr = new StringReader(sb.ToString());
+
+            Console.WriteLine("Reading the information...");
+
+            while (sr.Peek() > -1)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Thank you!");
+
+            sr.Close();
+        }
+    }
+}
---
+++ StringWriterAndStringReader/README.md
@@ -0,0 +1,11 @@
+﻿StringWriter and StringReader
+
+These classes provide the ability to manipulate string data inside a StringBuilder object
+- StringWriter writes to the StringBuilder object
+- StringReader reads string data from the StringBuilder object
+- Use them when you are dealing with a lot of string manipulations
+
+-------------------------------------------------------------------------------------------------------------
+Font:
+
+https://www.udemy.com/programming-in-microsoft-c-exam-70-483/
