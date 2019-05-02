9b07cb47d76eca21ff7ab6f552ac55c4d107121b
henriquesd <hsd.foz@gmail.com>
2019-05-02 09:18:38 -0300

    Async and Await

---
+++ AsyncAndAwait/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAndAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsyncAwaitDemo demo = new AsyncAwaitDemo();
+            demo.DoStuff();
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Working on the Main Thread........");
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    public class AsyncAwaitDemo
+    {
+        public async Task DoStuff()
+        {
+            // If we comment out the await Task.Run instructions and
+            // just call LongRunningOperation(), everything happens
+            // synchronously... 
+            // Program counts to 49, then writes 'Doing stuff..."
+            //
+            // Task.Run starts a background thread
+            // Await means this task must complete before the rest of this method is rn
+            await Task.Run(() =>
+                {
+                    CountToFifty();
+                });
+
+            // This will not execute until CountToFifty has completed;
+            Console.WriteLine("Counting to 50 is completed");
+        }
+
+        private static async Task<string> CountToFifty()
+        {
+            int counter;
+            for (counter = 0; counter < 51; counter++)
+            {
+                Console.WriteLine("Background thread: " + counter);
+            }
+
+            return "Counter = "  + counter;
+        }
+    }
+}
---
+++ AsyncAndAwait/README.md
@@ -0,0 +1,31 @@
+﻿Async and Await
+
+What's the difference between synchronous and asynchronous proccesing?
+- This is synchronous processing:
+	- Man walks into a bar, orders a drink, waits, gets drink, calss home
+- This is asynchronous processing:
+	- Man walks into bar, orders a drink, calls home while bartender prepares the drink
+
+
+Computers are designed to carry out actions synchronously
+- Complete task A, then complete task B
+
+Assynchronous processing makes this more efficient
+- Task A runs on the main thread
+- Task B runs on a separate thread at the same time
+
+Programming asynchronously is challenging
+- C# 5 introduced two keywords to simplify asynchronous processing
+	- Async and Await
+
+Async is used to mark a method to inform the compiler to look for the Await keyword
+- The await keyword tells the compiler that the Async method can't continue past that point until the awaited
+asynchronous process is complete
+- In the meantime, control returns to the caller of the Async method
+- Async caller and Await method run simultaneously
+
+
+-------------------------------------------------------------------------------------------------------------
+Font:
+
+https://www.udemy.com/programming-in-microsoft-c-exam-70-483/
