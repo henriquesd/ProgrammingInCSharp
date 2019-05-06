5c7f4b7f3d297f8b83ccc36411e21e45cf094e0d
henriquesd <hsd.foz@gmail.com>
2019-05-06 13:15:51 -0300

    Indexers

---
+++ Indexers/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c = new Car();
+            c[1] = "Mercedes";
+            c[2] = "Dodge";
+            c[3] = "Chevrolet";
+            c[4] = "Honda";
+
+            Console.WriteLine("Car 1 is: " + c[1]);
+            Console.WriteLine("Car 2 is: " + c[2]);
+            Console.WriteLine("Car 3 is: " + c[3]);
+            Console.WriteLine("Car 4 is: " + c[4]);
+            Console.WriteLine("Car 41 is: " + c[41]);
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Car
+    {
+        string[] car = new string[40];
+
+        public string this[int carNum]
+        {
+            get
+            {
+                if (carNum >= 0 && carNum < car.Length)
+                {
+                    return car[carNum];
+                }
+                return "Out of index range.";
+            }
+            set
+            {
+                if (carNum >= 0 && carNum < car.Length)
+                {
+                    car[carNum] = value;
+                }
+            }
+        }
+
+    }
+}
---
+++ Indexers/README.md
@@ -0,0 +1,14 @@
+﻿# Indexers
+
+Indexers allow instances of a class or struct to be indexed just like arrays. The indexed value can be set or retrieved without explicitly specifying a type or instance member.
+Indexers resemble properties except that their accessors take parameters.
+
+-------------------------------------------------------------------------------------------------------------
+An indexer provides the ability to access a type using an index similar to an array.
+
+
+-------------------------------------------------------------------------------------------------------------
+Fonts:
+
+https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/indexers/
+https://www.udemy.com/programming-in-microsoft-c-exam-70-483/
