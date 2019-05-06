781cefd8255fd315598ac7eba0b706be76eed4d9
henriquesd <hsd.foz@gmail.com>
2019-05-06 11:01:50 -0300

    Method Overloading

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Patient p = new Patient();
+            p.CheckBilling();
+            p.CheckBilling(1, 2);
+            p.CheckBilling("a", 2);
+
+            Child c = new Child();
+            c.CheckBilling("a", "b");
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Patient
+    {
+        public void CheckBilling()
+        {
+            Console.WriteLine("Patient: The billing has been checked.");
+        }
+
+        public void CheckBilling(int a, int b)
+        {
+            Console.WriteLine("Patient: The billing has been checked using two submitted integers.");
+        }
+
+        public void CheckBilling(string a, int b)
+        {
+            Console.WriteLine("Patient: The billing has been checked using a submitted string and integer.");
+        }
+    }
+
+    public class Child : Patient
+    {
+        public void CheckBilling(string a, string b)
+        {
+            Console.WriteLine("Child: The billing has been checked by submiting two strings.");
+        }
+    }
+}
---
+++ MethodOverloading/README.md
@@ -0,0 +1,19 @@
+﻿Method Overloading
+
+A method is a code block that contains a series of statements.
+
+Every method has a 'signature'
+- Just like your signature, it is unique to the method
+- Can be used to identify the method
+
+Multiple methods can be created using the same name
+- As long as the signatures are unique
+
+We can 'overload' methods based on their unique signatures
+- For the purpose of overloading, a method's signature is its name and the parameters it accepts
+- public string AddRecord(int a, string) (the method's signature is "AddRecord(int a, string b)")
+
+-------------------------------------------------------------------------------------------------------------
+Font:
+
+https://www.udemy.com/programming-in-microsoft-c-exam-70-483/
