ad28121ca7881a6294a12261b3cbbf49535468a9
henriquesd <hsd.foz@gmail.com>
2019-05-06 11:48:44 -0300

    Explicit Interface Implementation

---
+++ ExplicitInterfaceImplementation/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ExplicitInterfaceImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mark m = new Mark();
+            // m.DoThis();
+            ((IFirst)m).DoThis();
+
+            ((ISecond)m).DoThis();
+
+            // ISecond mm = new Mark();
+            // mm.DoThis();
+
+            Console.ReadLine();
+        }
+    }
+
+    interface IFirst
+    {
+        void DoThis();
+    }
+
+    interface ISecond
+    {
+        int DoThis();
+    }
+
+    public class Mark : IFirst, ISecond
+    {
+        public void DoThis()
+        {
+            Console.WriteLine("Implementation of IFirst.DoThis...");
+        }
+
+        int ISecond.DoThis()
+        {
+            Console.WriteLine("Implementation of ISecond.DoThis...");
+            return 5;
+        }
+
+        public void Test()
+        {
+            // add code;
+        }
+    }
+}
---
+++ ExplicitInterfaceImplementation/README.md
@@ -0,0 +1,15 @@
+﻿# Explicit Interface Implementation
+
+C# does not allow multiple inheritance
+- A class can only inherit from a single class
+
+C# does allow a class to implement multiple interfaces
+- This can sometimes result in a collision between two interface members
+
+Explicit interface implementation can be utilized to resolve the problem.
+
+
+-------------------------------------------------------------------------------------------------------------
+Font:
+
+https://www.udemy.com/programming-in-microsoft-c-exam-70-483/
