7e8b213ced9d7f0879df92b9637c13444d183b88
henriquesd <hsd.foz@gmail.com>
2019-05-06 11:15:17 -0300

    Override

---
+++ Override/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Override
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Patient p = new Patient();
+            p.Examine();
+
+            Adult a = new Adult();
+            a.Examine();
+
+            Patient pa = new Adult();
+            pa.Examine();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Patient
+    {
+        // The virtual keyword is used to modify a method, property, indexer, or event declaration
+        // and allow for it to be overridden in a derived class;
+        public virtual void Examine()
+        {
+            Console.WriteLine("The patient has been examined");
+        }
+    }
+
+    public class Adult : Patient
+    {
+        public override void Examine()
+        {
+            Console.WriteLine("The adult has been examined");
+        }
+    }
+}
---
+++ Override/README.md
@@ -0,0 +1,18 @@
+﻿# Override
+
+The override modifier is required to extend or modify the abstract or virtual implementation of an inherited method, property,
+indexer, or event.
+
+-------------------------------------------------------------------------------------------------------------
+
+- An override method provides a new implementation of a member that is inherited from a base class.
+- Overriding a method requires the use of two keywords
+	- The base method that is being replaced, or overriden, must include the keyword 'virtual'
+	- The overriding method must include the keyword 'override'
+
+
+-------------------------------------------------------------------------------------------------------------
+Fonts:
+
+https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/override
+https://www.udemy.com/programming-in-microsoft-c-exam-70-483/
