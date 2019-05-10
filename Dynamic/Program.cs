e604bd0f9878b7386b4d1b559713bd8e17d9fc66
henriquesd <hsd.foz@gmail.com>
2019-05-10 11:14:43 -0300

    Dynamic

---
+++ Dynamic/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Dynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ExampleUsingReflectionAndDynamic();
+            // Console.WriteLine("-----------------------------");
+
+            ExampleDynamic();
+
+            Console.ReadLine();
+        }
+
+        private static void ExampleDynamic()
+        {
+            dynamic name = "Henrique";
+            name = 10;
+
+            Console.WriteLine(name);
+
+            // the code below will throw an error:
+            //dynamic name2 = "henrique";
+            //name2++;
+            //Console.WriteLine(name2);
+
+            dynamic a = 10;
+            dynamic b = 5;
+            var c = a + b;
+            Console.WriteLine(c);
+
+            // Conversion and cast;
+            int i = 5;
+            dynamic d = i;
+            Console.WriteLine(d);
+
+            long l = d;
+            Console.WriteLine(l);
+
+
+        }
+
+        private static void ExampleUsingReflectionAndDynamic()
+        {
+            object obj = "Henrique";
+
+            // reflection; (Reflection is a way to inspect the metadata about the type, and access properties and methods);
+            // obj.GetHashCode();
+            var methodInfo = obj.GetType().GetMethod("GetHashCode");
+            methodInfo.Invoke(null, null);
+
+            // dynamic is much cleaner than reflection;
+            dynamic excelObject = "henrique";
+            excelObject.Optimize();
+
+        }
+    }
+}
---
+++ Dynamic/README.md
@@ -0,0 +1,25 @@
+﻿# Dynamic
+
+Programming languages are divided into two types:
+- Statically-typed languages: C#, Java
+- Dynamically-typed languages: Ruby, JavaScript, Python
+
+What's the difference?
+	Type Resolution
+	- Static languages: at compile-time
+		- In static languages - resolution of types, members, properties, methods, is done at compile-time. So if you try to
+		access a method that is not defined in an object, when we compile the application, we are getting an immediate feedback
+		that's telling us that method or property is not found on that object.
+	- Dynamic languages: at run-time
+		- With dynamic languages - the resolution of types, properties, members, methods, is done at run-time.
+
+Benefits
+- Static languages: early feedback (compile-time).
+- Dynamic languages: easier and faster to code (but on the flip side, because we loose compile-time checking, we have
+to write more unit tests to make sure the application behaves properly at run-time).
+
+
+-------------------------------------------------------------------------------------------------------------
+Font:
+
+https://www.udemy.com/csharp-advanced/
